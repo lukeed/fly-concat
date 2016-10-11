77ff4b6651f2605091f82729fe8dbbed4868802d
Luke Edwards <luke.edwards05@gmail.com>
2016-10-10 20:44:45 -0700

    declare module logic

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
 'use strict';
-module.exports = function (str, opts) {
-	if (typeof str !== 'string') {
-		throw new TypeError('Expected a string');
-	}
 
-	opts = opts || {};
+const resolve = require('path').resolve;
+const Concat = require('concat-with-sourcemaps');
 
-	return str + ' & ' + (opts.postfix || 'rainbows');
+const defs = {
+	sep: '',
+	base: '',
+	maps: false,
+	output: null
+};
+
+module.exports = function () {
+	this.plugin('concat', {every: 0}, function * (arr, o) {
+		if (typeof o === 'string') {
+			o = {output: o};
+		}
+
+		o = Object.assign({}, defs, o);
+
+		if (!o.output) {
+			throw new Error('`fly-concat` did not receive an `output` filename.');
+		}
+
+		const bundle = new Concat(o.maps, o.output, o.sep);
+
+		for (const file of arr) {
+			bundle.add(file.base, file.data, file.map || file.sourceMap);
+		}
+
+		// if did not specify a `base`, assume first file's location
+		const dir = o.base ? resolve(this.root, o.base) : arr[0].dir;
+		// concat'd content
+		let data = bundle.content;
+
+		// reset
+		this._.files = [];
+
+		if (o.maps && bundle.sourceMap) {
+			const mapFile = o.output.concat('.map');
+			// add link to sourcemap file
+			data += new Buffer(`\n//# sourceMappingURL=${mapFile}`);
+			// add sourcemap file definition
+			this._.files.push({
+				dir: dir,
+				base: mapFile,
+				data: bundle.sourceMap
+			});
+		}
+
+		// always add the concat'd file
+		this._.files.push({
+			dir: dir,
+			base: o.output,
+			data: data
+		});
+	});
 };
